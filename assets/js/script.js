14b2dcafcd9fe88aaf8e3013f9571f249566edf7
Felipe Menezes <fmm312@gmail.com>
2018-01-21 11:58:20 -0300

    bugs correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,36 @@
 /* som ao clicar em cada nota */
-$('div').click(function(){
-	var classe = $(this).attr('class').split(' ')[0];
+$('.teclado>.teclas>div').click(function(){
+	var id = $(this).attr('id');
 	var notaSom = new Audio();
-    notaSom.src = 'assets/audios/'+classe+'.mp3';
+    notaSom.src = 'assets/audios/'+ id +'.mp3';
     notaSom.play();
 });
+
+$('.teclado>.teclas>div>div').click(function(){
+    id = $(this).attr('id');
+    notaSom = new Audio();
+    notaSom.src = 'assets/audios/'+ id +'.mp3';
+    notaSom.play();
+});
+
 
 /* efeito no css ao clicar na nota */
 $('.nota').mousedown(function(){
     $(this).css({'height' : '295px' , 'box-shadow' : 'inset 0 0 20px #333333' });
 });
- $('.nota').mouseout(function(){
+
+$('.nota').mouseout(function(){
     $(this).css({'height' : '300px' , 'box-shadow' : 'inset 0 0 10px #333333' });
  });
- $('.sustenido').mousedown(function(e){
+
+$('.sustenido').mousedown(function(e){
  	var e = e || window.event;
 	e.stopPropagation();
     $(this).css('border-width' , '1px 3px 4px');
 });
- $('.sustenido').mouseout(function(e){
+
+$('.sustenido').mouseout(function(e){
  	var e = e || window.event;
 	e.stopPropagation();
     $(this).css('border-width' , '1px 3px 8px');
- });
+});
